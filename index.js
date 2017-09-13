8d7935ce23f60ee0592ed9323ef1273f727458ad
Nathan Gezahegne <nathan.daniel5719@gmail.com>
2017-09-13 00:25:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,15 @@
 function happyHolidays(){
   return "happy Holidays!"
 }
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo(holiday, name){
+    return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(holiday,days) {
+  return `It's ${days} days until ${holiday}!`
+}
